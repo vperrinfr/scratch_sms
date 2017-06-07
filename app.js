e672f1cb62db8158668a3eddd48d7901467ef64a
Vincent Perrin <vincent.perrin@fr.ibm.com>
2017-06-07 22:37:40 +0200

    first commit

--- app.js
+++ app.js
@@ -1,43 +1,14 @@
-/*eslint-env node*/
+var express = require("express"),
+    app = express();
 
-//------------------------------------------------------------------------------
-// node.js starter application for Bluemix
-//------------------------------------------------------------------------------
+var port = process.env.PORT || 8080;
 
-// This application uses express as its web server
-// for more info, see: http://expressjs.com
-var express = require('express');
-var http   = require('http');
-var bwipjs = require('bwip-js');
+app.use(express.static(__dirname + '/public'));
 
-// cfenv provides access to your Cloud Foundry environment
-// for more info, see: https://www.npmjs.com/package/cfenv
-var cfenv = require('cfenv');
-
-// create a new express server
-var app = express();
-bwipjs.loadFont('Inconsolata', 108,
-            require('fs').readFileSync('fonts/Inconsolata.otf', 'binary'));
-
-var server = http.createServer(function(req, res) {
-	// If the url does not begin /?bcid= then 404.  Otherwise, we end up
-	// returning 400 on requests like favicon.ico.
-	if (req.url.indexOf('/?bcid=') != 0) {
-		res.writeHead(404, { 'Content-Type':'text/plain' });
-		res.end('BWIP-JS: Unknown request format.', 'utf8');
-	} else {
-		// Do not allow images to grow too large (1 mega-pixel limit)
-		bwipjs(req, res, { sizelimit:1024*1024 });
-	}
-
-})			
-			
-			
-// get the app environment from Cloud Foundry
-var appEnv = cfenv.getAppEnv();
-
-// start server on the specified port and binding host
-server.listen(appEnv.port, '0.0.0.0', function() {
-  // print a message when the server starts listening
-  console.log("server starting on " + appEnv.url);
+app.get("/sayHello", function (request, response) {
+  var user_name = request.query.user_name;
+  response.end("Hello " + user_name + "!");
 });
+
+app.listen(port);
+console.log("Listening on port ", port);
